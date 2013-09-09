95c5ad23c41bec324b77e51c9b0ef580319e8948
Francisco Javier Sánchez Rosales <aslanlion56@gmail.com>
2013-09-09 12:50:47 -0500

    Complemento de login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,27 +1,46 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
-	{
-		$this->load->view('login');
-	}
-}
+	{	
+		
+		$this->form_validation->set_rules('user_name', 'Username', 'strip_tags|trim|valid_email|required|xss_clean');
+		$this->form_validation->set_rules('password', 'Password', 'strip_tags|trim|required|md5');
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+		$data['title'] = "Login";
+		$data['view']  = "login";
+
+		if ($this->form_validation->run() === false){
+
+			$data['error_message'] = "Inserte su nombre de usuario y contraseña por favor";
+			$this->load->view('template', $data);
+
+		} else {
+
+			$name      = $this->input->post('user_name');
+			$password  = $this->input->post('password');
+			
+			$this->load->model('login_model');
+			$result = $this->login_model->get_user($name, $password);
+
+			if ($result === true){
+
+				redirect(base_url().'welcome');
+
+			}else{
+
+				$data['error_menssage'] = 'Usuario y/o constraseña invalido';
+				$this->load->view('template', $data);
+
+			}
+		}
+	}
+
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect(base_url().'login');
+	}
+
+}
