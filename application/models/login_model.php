ee4b04bf17fa7fb3dc638d4eb193ebb98851d7c0
Francisco Javier Sánchez Rosales <aslanlion56@gmail.com>
2013-09-30 15:08:27 -0500

    Se agreaga funacion privada _validate_entry_output para validar entrada y salida

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -9,43 +9,60 @@ class Login_model extends CI_Model {
 
 		if ($query->num_rows() > 0) {
 
-			$query       = $query->row_array();
-			$user_name   = $query['username'];
-			$password_db = $query['password'];
-			$id_user     = $query['id'];
+			$query    = $query->row_array();
+			$id_user  = $query['id'];
 
-			$this->db->where('users_id'   , $id_user);
-    		$this->db->where('YEAR(date)' , date("Y"));
-    		$this->db->where('MONTH(date)', date("m"));
-    		$this->db->where('DAY(date)'  , date("d"));
-    		$this->db->where('type'       , $type);
-    		$query = $this->db->get('assistance');
+    		if ($this->_validate_entry_output($id_user, $type) === true){
 
-    		if ($query->num_rows() == 0){
-				$this->_add_assistance($id_user);
-				return true;
-			} else {
-				return false;
+					$this->_add_assistance($id_user, $type);
+					return true;
 			}
+
+		} else {
+				return false;
+		}
+	}
+
+	private function _validate_entry_output ($id_user, $type)
+	{	
+		$this->db->where('users_id'   , $id_user);
+    	$this->db->where('YEAR(date)' , date("Y"));
+		$this->db->where('MONTH(date)', date("m"));
+		$this->db->where('DAY(date)'  , date("d"));
+		$this->db->where('type'       , $type);
+
+		$query = $this->db->get('assistance');
+
+		if ($query->num_rows() > 0) {
+			return false;
+		} else {
+			if ($type == 0){
+				$this->db->where('users_id'   , $id_user);
+		    	$this->db->where('YEAR(date)' , date("Y"));
+				$this->db->where('MONTH(date)', date("m"));
+				$this->db->where('DAY(date)'  , date("d"));
+				$this->db->where('type'       , 1);
+
+				$query = $this->db->get('assistance');
+
+				if ($query->num_rows() > 0) {
+					return true;
+				} else {
+					return false;
+				}
+
+			}
+			return true;
 		}
 
 	}
 
-	private function _valid_entry ($id_user)
-	{
-		
-	}
-
-	private function _valid_output ($id_user)
-	{
-
-	}
-
-	private function _add_assistance ($id_user)
+	private function _add_assistance ($id_user, $type)
 	{
 		$date = date("Y-m-d H:i:s"); 
 		$this->db->set("users_id", $id_user);
 		$this->db->set("date", $date);
+		$this->db->set("type", $type);
 		$this->db->insert("assistance");
 	}
 
